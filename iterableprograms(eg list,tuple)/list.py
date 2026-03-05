f7ac731d2fa0140923ab24bf5c13cc546653dc57
rashi <rashipardeshi870@gmail.com>
2026-03-05 23:15:38 +0530

    Adding list programs

---
+++ iterableprograms(eg list,tuple)/list.py
@@ -0,0 +1,50 @@
+#1- Write a program to access the third element from the given list. Input: [10, 20, 30, 40, 50]
+
+li=[10, 20, 30, 40, 50]
+print(li[2])
+
+#2 Write a program to access the last element from the given list using negative indexing. Input: ['apple', 'banana', 'cherry', 'date']
+
+fruits= ['apple', 'banana', 'cherry', 'date']
+print(fruits[-1])
+
+#3 Write a program to change the second element to 100 in the given list. Input: [5, 10, 15, 20, 25]
+
+li= [5, 10, 15, 20, 25]
+li[1]=100
+print(li)
+
+#4 Write a program to access the first element using negative indexing. Input: [7, 14, 21, 28, 35]
+
+li=[7, 14, 21, 28, 35]
+print(li[-5])
+
+#5 Write a program to access the second last element from the given list. Input: [1, 2, 3, 4, 5, 6]
+
+li=[1, 2, 3, 4, 5, 6]
+print(li[-2])
+
+#6 Write a program to extract the first three elements from the given list. Input: [100, 200, 300, 400, 500, 600]
+
+ele=[100, 200, 300, 400, 500, 600]
+print(ele[0:3])
+
+#7 Write a program to extract all elements except the first two from the given list. Input: [2, 4, 6, 8, 10, 12]
+
+li=[2, 4, 6, 8, 10, 12]
+print(li[2::])
+
+#8 Write a program to extract every alternate element from the given list. Input: [5, 10, 15, 20, 25, 30, 35]
+
+li=[5, 10, 15, 20, 25, 30, 35]
+print(li[::2])
+
+#9 Write a program to reverse the given list using slicing. Input: [1, 3, 5, 7, 9]
+
+li=[1, 3, 5, 7, 9]
+print(li[::-1])
+
+#10 Write a program to extract elements from index 2 to 5 from the given list. Input: [11, 22, 33, 44, 55, 66, 77]
+
+li= [11, 22, 33, 44, 55, 66, 77]
+print(li[2:6])
